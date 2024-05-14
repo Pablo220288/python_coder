868aa886ed655bae9601236a784a0900912e9525
Pablo <peh_tj@hotmail.com>
2024-05-14 14:04:25 -0300

    formatter

--- def.py
+++ def.py
@@ -1,69 +1,77 @@
-
 data = {}
+
 
 # Abrimos el TXT y cargamos los valores existentes en la DB
 def upload():
-  with open('DB.txt',"r") as archivo:
-    step = ":"
-    for linea in archivo:
-        key, value = linea.split(step)
-        data[key.strip()] = value.strip()
+    with open("DB.txt", "r") as archivo:
+        step = ":"
+        for linea in archivo:
+            key, value = linea.split(step)
+            data[key.strip()] = value.strip()
+
 
 # Funcion para Agergar Usuarios a la DB
 def add():
-   with open('DB.txt',"a") as adduser:
-      for key, value in data.items():
-         adduser.write("%s: %s\n" %(key, value) )
+    with open("DB.txt", "a") as adduser:
+        for key, value in data.items():
+            adduser.write("%s: %s\n" % (key, value))
+
 
 # Funcion de Consulta de Usuarios
 def get_user():
-   upload()
-   return print(data)
+    upload()
+    return print(data)
+
 
 # Funcion de Registro de Usuarios
 def register():
-  upload()
+    upload()
 
-  user = input("Ingrese un Usuario :")
-  if user in data.keys():
-    print("El usuario ya exite. Inicie Secion")
-    return
+    user = input("Ingrese un Usuario :")
+    if user in data.keys():
+        print("El usuario ya exite. Inicie Secion")
+        return
 
-  password = input("Ingrese un Password :")
-  data[user]=password
+    password = input("Ingrese un Password :")
+    data[user] = password
 
-  print("Usuario creado Exitosamente")
-  add()
+    print("Usuario creado Exitosamente")
+    add()
 
-  return(get_user())
+    return get_user()
+
 
 # Funcion de Login de Usuarios
 def login():
-   upload()
+    upload()
 
-   attempts_limit = 3
-   attempts = 0
+    attempts_limit = 3
+    attempts = 0
 
-   for i in range(0, attempts_limit):
-      user = input("Ingrese su Usuario :")
-      if user in data.keys():
-         for i in range(0, attempts_limit):
-            password = input("Ingrese su Password :")
-            if password == data[user]:
-               return print("Login de Usuario Exitoso")
+    for i in range(0, attempts_limit):
+        user = input("Ingrese su Usuario :")
+        if user in data.keys():
+            for i in range(0, attempts_limit):
+                password = input("Ingrese su Password :")
+                if password == data[user]:
+                    return print("Login de Usuario Exitoso")
+                else:
+                    attempts += 1
+                    if attempts == attempts_limit:
+                        return print("Supero la Cantidad de intentos fallidos")
+                    else:
+                        print(
+                            f"Password Incorrecto. Intentelo de Nuevo. Te quedan {attempts_limit - attempts} intentos"
+                        )
+        else:
+            attempts += 1
+            if attempts == attempts_limit:
+                return print("Supero la Cantidad de intentos fallidos")
             else:
-               attempts += 1
-               if(attempts == attempts_limit):   
-                  return print("Supero la Cantidad de intentos fallidos")
-               else:
-                  print(f"Password Incorrecto. Intentelo de Nuevo. Te quedan {attempts_limit - attempts} intentos")
-      else:
-         attempts += 1
-         if(attempts == attempts_limit):
-            return print("Supero la Cantidad de intentos fallidos")
-         else:
-            print(f"Usuario Incorrecto. Intentelo de Nuevo. Te quedan {attempts_limit - attempts} intentos")
-         
+                print(
+                    f"Usuario Incorrecto. Intentelo de Nuevo. Te quedan {attempts_limit - attempts} intentos"
+                )
+
 
 register()
 # get_user()
